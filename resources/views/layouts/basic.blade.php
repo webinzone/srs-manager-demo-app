690102ed4fc7895c8a50cb6088b028f4601215ee
asn-athira <athira@rightsolution.ae>
2021-06-01 13:09:17 +0530

    modified

--- resources/views/layouts/basic.blade.php
+++ resources/views/layouts/basic.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>SRS MANAGER</title>
+    <title>Residential Care Manager</title>
 
     <link rel="shortcut icon" type="image/ico" href="{{ ($snipeSettings) && ($snipeSettings->favicon!='') ?  Storage::disk('public')->url('').e($snipeSettings->favicon) : 'favicon.ico' }} ">
     {{-- stylesheets --}}
