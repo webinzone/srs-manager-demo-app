0fb111f11b46440863daca3e5bff4e3da998881d
asn-athira <athira@rightsolution.ae>
2021-07-27 23:10:19 +0530

    fixed

---
+++ database/migrations/2021_07_27_230756_alter_table_referal.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableReferal extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::table('referrals', function (Blueprint $table) {
+            
+            $table->string('client_id')->nullable()->default(null);
+
+            
+
+       });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+       Schema::table('referrals', function (Blueprint $table) {
+            
+            $table->dropColumn('client_id');
+
+            
+
+       });
+    }
+}
