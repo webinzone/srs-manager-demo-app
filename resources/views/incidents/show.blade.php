6d4f079a3fd3616ef385f02a154f93d26bd663cb
asn-athira <athira@rightsolution.ae>
2021-07-29 00:41:11 +0530

    modified

--- resources/views/incidents/show.blade.php
+++ resources/views/incidents/show.blade.php
@@ -14,170 +14,146 @@ Incidents
 
 {{-- Page content --}}
 @section('content')
+<style type="text/css">
+      table, td, th {
+    border: 1px solid black;
+  }
+  td{
+    width: 200px;
+  }
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    padding: 10px;
+  }
+</style>
 
   <div id="webui">
-    <div class="row">
+    <div class="row" style="padding-left: 80px;padding-right: 80px;">
         <!-- left column -->
       <div class="col-md-7">
-        <form class="form-horizontal" method="" action="" autocomplete="off">
+        <form class="form-horizontal" method="" action="" autocomplete="off" style="width: 1000px; align-items: center;   background-color: #fff; padding-right: 100px;">
           <div class="box box-default">
-            <div class="box-header with-border">
-                <h2 class="box-title"> Incident</h2>
-            </div>
+              <div class="box-header with-border text-center">
+                 <h3><b>Incident Report</b></h3>
+                   
+                </div><!-- /.box-header -->
+           
 
-            <div class="box-body">
+            <div class="box-body" style="padding-left:80px;">
 
-                <!-- Asset name -->
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Incident Date:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->i_date}}</p>
-                    </div>
-                </div>
-               <div class="form-group">
-                    <label class="col-sm-3 control-label">Incident Time:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->i_time}}</p>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Staff Name:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->s_name}}</p>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Staff Sign:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->s_sign}}</p>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Person/involved effected in incident:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->p_name}}</p>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Location:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->place}}</p>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Doctor:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->doctor}}</p>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">NOK:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->nok}}</p>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Case Mgr:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->case_mgr}}</p>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Management:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->management}}</p>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">DHSS:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->dhss}}</p>
-                    </div>
-                </div> 
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Notified Date:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->n_date}}</p>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Notified Time:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->n_time}}</p>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Resident transfferd to the hospital:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->res_hos}}</p>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Incident Details:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->i_details}}</p>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Action Taken:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->actions}}</p>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Action Date:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->action_date}}</p>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Other Details:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->o_det}}</p>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Prescribed reportable incident:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->i_prescribed}}</p>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Police notified:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->police_noti}}</p>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Does the resident’s support plan need updating:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->sp_update}}</p>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Reported to the Department:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->reported}}</p>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Authorized Officer’s Name:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->auth_name}}</p>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Reported Date:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->rep_date}}</p>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3 control-label">Reported Time:</label>
-                    <div class="col-md-6">
-                        <p class="form-control-static">{{ $incident->rep_time}}</p>
-                    </div>
-                </div>    
+               <table style="border: 1px solid black;">
+      <tr style="border: 1px solid black;">
+        <td style="border: 1px solid black;">Incident Date</td>
+        <td style="border: 1px solid black;">Incident Time</td>
+        <td style="border: 1px solid black;">Person/involved effected</td>
+      </tr>
+      <tr style="border: 1px solid black;">
+        <td style="border: 1px solid black;">{{date('d-m-Y', strtotime($incident->i_date)) }}</td>
+        <td style="border: 1px solid black;">{{ $incident->i_time}}</td>
+        <td style="border: 1px solid black;">{{ $incident->p_name}}</td>
+      </tr>
+    </table><br>
+
+    <table style="border: 1px solid black;">
+      <tr style="border: 1px solid black;">
+        <td style="border: 1px solid black;">Staff Name</td>
+        <td style="border: 1px solid black;">{{ $incident->s_name}}</td>        
+        <td style="border: 1px solid black;">Location</td>
+         <td style="border: 1px solid black;">{{ $incident->place}}</td>
+      </tr>
+      
+    </table><br>
+
+    <table style="border: 1px solid black;">
+      
+       <tr style="border: 1px solid black;">
+        <td style="border: 1px solid black;">Persons notified</td>
+         <td style="border: 1px solid black;">{{ $incident->doctor}}</td>
+       <td style="border: 1px solid black;">Notified Date</td>
+       <td style="border: 1px solid black;">{{date('d-m-Y', strtotime($incident->n_date)) }}</td>
+        <td style="border: 1px solid black;">Notified Time</td>
+        <td style="border: 1px solid black;">{{ $incident->n_time}}</td>
+      </tr>
+       
+    </table><br>
+     
+    <table style="border: 1px solid black;">
+      <tr style="border: 1px solid black;">
+        <td style="border: 1px solid black;" width="300px">Has Resident transfferd to the hospital?</td>
+        <td style="border: 1px solid black;" width="100px">{{ $incident->res_hos}}</td>
+        <td style="border: 1px solid black;" width="200px">Incident Details</td>
+        <td style="border: 1px solid black;">{{ $incident->i_details}}</td>
+      </tr>
+      <tr style="border: 1px solid black;">
+        
+        
+      </tr>
+    </table><br>
+
+    
+     <table style="border: 1px solid black;">
+      <tr style="border: 1px solid black;">
+       <td style="border: 1px solid black;" width="200px">Action Date</td>
+       <td style="border: 1px solid black;" width="200px">{{date('d-m-Y', strtotime($incident->action_date)) }}</td>
+        <td style="border: 1px solid black;" width="200px">Actions taken</td>
+        <td style="border: 1px solid black;" width="200px">{{ $incident->actions}}</td>
+      </tr>
+      <tr style="border: 1px solid black;">
+        
+        
+      </tr>
+    </table><br>
+
+    <table style="border: 1px solid black;">
+      <tr style="border: 1px solid black;">
+        <td style="border: 1px solid black;">Follow up Needed</td>
+        <td style="border: 1px solid black;">{{ $incident->need}}</td>
+        <td style="border: 1px solid black;">Other details</td>
+        <td style="border: 1px solid black;">{{ $incident->o_det}}</td>
+      </tr>
+     
+    </table><br>
+    
+    <table style="border: 1px solid black;">
+      <tr style="border: 1px solid black;">
+        <td style="border: 1px solid black;" width="300px">Prescribed reportable incident</td>
+        <td style="border: 1px solid black;">{{ $incident->i_prescribed}}</td>
+      </tr>
+    
+    </table><br>
+    
+    <table style="border: 1px solid black;">
+      <tr style="border: 1px solid black;">
+        <td style="border: 1px solid black;">Police Notified</td>
+        <td style="border: 1px solid black;">Does the resident’s support plan need updating?</td>
+      </tr>
+      <tr style="border: 1px solid black;">
+        <td style="border: 1px solid black;">{{ $incident->police_noti}}</td>
+        <td style="border: 1px solid black;">{{ $incident->sp_update}}</td>
+      </tr>
+    </table><br>
+
+    <table style="border: 1px solid black;">
+      <tr style="border: 1px solid black;">
+        <td style="border: 1px solid black;">Reported to the Department</td>
+        <td style="border: 1px solid black;">Authorized Officer’s Name</td>
+      </tr>
+      <tr style="border: 1px solid black;">
+        <td style="border: 1px solid black;">{{ $incident->reported}}</td>
+        <td style="border: 1px solid black;">{{ $incident->auth_name}}</td>
+      </tr>
+    </table><br>
+    <table style="border: 1px solid black;">
+      <tr style="border: 1px solid black;">
+       <td style="border: 1px solid black;">Reported Date</td>
+        <td style="border: 1px solid black;">Reported Time</td>
+      </tr>
+      <tr style="border: 1px solid black;">
+        <td style="border: 1px solid black;">{{date('d-m-Y', strtotime($incident->rep_date)) }}</td>
+        <td style="border: 1px solid black;">{{ $incident->rep_time}}</td>
+      </tr>
+    </table><br>
           
             </div>
           </div>
