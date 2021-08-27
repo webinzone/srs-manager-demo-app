4ed9c6ddd9238c984f716cae03f55da6002c9096
asn-athira <athira@rightsolution.ae>
2021-08-27 22:22:02 +0530

    modified

--- database/seeds/SupplierSeeder.php
+++ database/seeds/SupplierSeeder.php
@@ -6,8 +6,8 @@ class SupplierSeeder extends Seeder
 {
     public function run()
     {
-        Supplier::truncate();
-        factory(Supplier::class, 5)->create();
+        //Supplier::truncate();
+       // factory(Supplier::class, 5)->create();
 
     }
 }
