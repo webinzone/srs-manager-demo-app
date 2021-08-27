2c7427e5564a5bae9d76b119f44388ee37004193
asn-athira <athira@rightsolution.ae>
2021-08-27 22:43:15 +0530

    modified

--- database/seeds/LicenseSeeder.php
+++ database/seeds/LicenseSeeder.php
@@ -7,11 +7,11 @@ class LicenseSeeder extends Seeder
 {
     public function run()
     {
-        License::truncate();
-        LicenseSeat::truncate();
-        factory(License::class, 1)->states('photoshop')->create();
-        factory(License::class, 1)->states('acrobat')->create();
-        factory(License::class, 1)->states('indesign')->create();
-        factory(License::class, 1)->states('office')->create();
+        //License::truncate();
+        //LicenseSeat::truncate();
+        //factory(License::class, 1)->states('photoshop')->create();
+        //factory(License::class, 1)->states('acrobat')->create();
+        //factory(License::class, 1)->states('indesign')->create();
+        //factory(License::class, 1)->states('office')->create();
     }
 }
