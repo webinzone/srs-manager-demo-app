a634c843a7cb8bd657b641dcfc6fde7cab81801f
asn-athira <athira@rightsolution.ae>
2021-08-27 18:49:14 +0530

    modified

--- database/seeds/CompanySeeder.php
+++ database/seeds/CompanySeeder.php
@@ -14,34 +14,34 @@ class CompanySeeder extends Seeder
     {
         \Log::debug('Seed companies');
         Company::truncate();
-        factory(Company::class, 4)->create();
-
-
-        $src = public_path('/img/demo/companies/');
-        $dst = 'companies'.'/';
-        $del_files = Storage::files('companies/'.$dst);
-
-        foreach($del_files as $del_file){ // iterate files
-            $file_to_delete = str_replace($src,'',$del_file);
-            \Log::debug('Deleting: '.$file_to_delete);
-            try  {
-                Storage::disk('public')->delete($dst.$del_file);
-            } catch (\Exception $e) {
-                \Log::debug($e);
-            }
-        }
-
-
-        $add_files = glob($src."/*.*");
-        foreach($add_files as $add_file){
-            $file_to_copy = str_replace($src,'',$add_file);
-            \Log::debug('Copying: '.$file_to_copy);
-            try  {
-                Storage::disk('public')->put($dst.$file_to_copy, file_get_contents($src.$file_to_copy));
-            } catch (\Exception $e) {
-                \Log::debug($e);
-            }
-        }
+        //factory(Company::class, 4)->create();
+//
+//
+        //$src = public_path('/img/demo/companies/');
+        //$dst = 'companies'.'/';
+        //$del_files = Storage::files('companies/'.$dst);
+//
+        //foreach($del_files as $del_file){ // iterate files
+            //$file_to_delete = str_replace($src,'',$del_file);
+            //\Log::debug('Deleting: '.$file_to_delete);
+            //try  {
+                //Storage::disk('public')->delete($dst.$del_file);
+            //} catch (\Exception $e) {
+                //\Log::debug($e);
+            //}
+        //}
+//
+//
+        //$add_files = glob($src."/*.*");
+        //foreach($add_files as $add_file){
+            //$file_to_copy = str_replace($src,'',$add_file);
+            //\Log::debug('Copying: '.$file_to_copy);
+            //try  {
+                Storage::disk('public')->put($dst.$file_to_copy, file_get_contents($src.$file_to_copy//));
+            //} catch (\Exception $e) {
+                //\Log::debug($e);
+            //}
+        //}
 
 
     }
