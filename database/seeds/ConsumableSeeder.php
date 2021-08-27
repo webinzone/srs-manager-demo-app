2ed7286ec5d71e1b4bec6b66c59f68a6fa851d9c
asn-athira <athira@rightsolution.ae>
2021-08-27 22:48:56 +0530

    modified

--- database/seeds/ConsumableSeeder.php
+++ database/seeds/ConsumableSeeder.php
@@ -6,10 +6,10 @@ class ConsumableSeeder extends Seeder
 {
     public function run()
     {
-        Consumable::truncate();
-        DB::table('consumables_users')->truncate();
-        factory(Consumable::class, 1)->states('cardstock')->create(); // 1
-        factory(Consumable::class, 1)->states('paper')->create(); // 2
-        factory(Consumable::class, 1)->states('ink')->create(); // 3
+        //Consumable::truncate();
+        //DB::table('consumables_users')->truncate();
+        //factory(Consumable::class, 1)->states('cardstock')->create(); // 1
+        //factory(Consumable::class, 1)->states('paper')->create(); // 2
+        //factory(Consumable::class, 1)->states('ink')->create(); // 3
     }
 }
