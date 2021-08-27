a1b11d4ec7ca0a303783bb50024674f914246ade
asn-athira <athira@rightsolution.ae>
2021-08-27 22:50:06 +0530

    modified

--- database/seeds/CustomFieldSeeder.php
+++ database/seeds/CustomFieldSeeder.php
@@ -10,62 +10,62 @@ class CustomFieldSeeder extends Seeder
 {
   public function run()
   {
-      $columns = DB::getSchemaBuilder()->getColumnListing('assets');
-
-
-          foreach ($columns as $column) {
-              if(strpos($column, '_snipeit_') !== FALSE) {
-
-                  Schema::table('assets', function (Blueprint $table) use ($column) {
-                      $table->dropColumn($column);
-                  });
-              }
-          }
-      CustomField::truncate();
-      CustomFieldset::truncate();
-      DB::table('custom_field_custom_fieldset')->truncate();
-
-      factory(CustomFieldset::class, 1)->states('mobile')->create();
-      factory(CustomFieldset::class, 1)->states('computer')->create();
-      factory(CustomField::class, 1)->states('imei')->create();
-      factory(CustomField::class, 1)->states('phone')->create();
-      factory(CustomField::class, 1)->states('ram')->create();
-      factory(CustomField::class, 1)->states('cpu')->create();
-      factory(CustomField::class, 1)->states('mac-address')->create();
-
-      DB::table('custom_field_custom_fieldset')->insert([
-            [
-                'custom_field_id' => '1',
-                'custom_fieldset_id' => '1',
-                'order' => '2',
-                'required' => true
-            ],
-            [
-                'custom_field_id' => '2',
-                'custom_fieldset_id' => '1',
-                'order' => '2',
-                'required' => true
-            ],
-            [
-              'custom_field_id' => '3',
-              'custom_fieldset_id' => '2',
-                'order' => '1',
-                'required' => true
-            ],
-            [
-              'custom_field_id' => '4',
-              'custom_fieldset_id' => '2',
-                'order' => '1',
-                'required' => true
-            ],
-            [
-              'custom_field_id' => '5',
-              'custom_fieldset_id' => '2',
-                'order' => '1',
-                'required' => true
-            ],
-
-      ]);
+      //$columns = DB::getSchemaBuilder()->getColumnListing('assets');
+//
+//
+          //foreach ($columns as $column) {
+              //if(strpos($column, '_snipeit_') !== FALSE) {
+//
+                  //Schema::table('assets', function (Blueprint $table) use ($column) {
+                      //$table->dropColumn($column);
+                  //});
+              //}
+          //}
+      //CustomField::truncate();
+      //CustomFieldset::truncate();
+      //DB::table('custom_field_custom_fieldset')->truncate();
+//
+      //factory(CustomFieldset::class, 1)->states('mobile')->create();
+      //factory(CustomFieldset::class, 1)->states('computer')->create();
+      //factory(CustomField::class, 1)->states('imei')->create();
+      //factory(CustomField::class, 1)->states('phone')->create();
+      //factory(CustomField::class, 1)->states('ram')->create();
+      //factory(CustomField::class, 1)->states('cpu')->create();
+      //factory(CustomField::class, 1)->states('mac-address')->create();
+//
+      //DB::table('custom_field_custom_fieldset')->insert([
+            //[
+                //'custom_field_id' => '1',
+                //'custom_fieldset_id' => '1',
+                //'order' => '2',
+                //'required' => true
+            //],
+            //[
+                //'custom_field_id' => '2',
+                //'custom_fieldset_id' => '1',
+                //'order' => '2',
+                //'required' => true
+            //],
+            //[
+              //'custom_field_id' => '3',
+              //'custom_fieldset_id' => '2',
+                //'order' => '1',
+                //'required' => true
+            //],
+            //[
+              //'custom_field_id' => '4',
+              //'custom_fieldset_id' => '2',
+                //'order' => '1',
+                //'required' => true
+            //],
+            //[
+              //'custom_field_id' => '5',
+              //'custom_fieldset_id' => '2',
+                //'order' => '1',
+                //'required' => true
+            //],
+//
+      //]);
 
 
 
