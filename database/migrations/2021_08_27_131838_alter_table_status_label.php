3d87014dcc4ad3d57725a20d42e4becde08a8426
asn-athira <athira@rightsolution.ae>
2021-08-27 13:23:09 +0530

    modified

---
+++ database/migrations/2021_08_27_131838_alter_table_status_label.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableStatusLabel extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::table('status_labels', function (Blueprint $table) {
+            $table->boolean('c_id')->nullable();
+          
+       });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('status_labels', function (Blueprint $table) {
+            $table->dropColumn('c_id')->nullable();
+          
+       });
+    }
+}
